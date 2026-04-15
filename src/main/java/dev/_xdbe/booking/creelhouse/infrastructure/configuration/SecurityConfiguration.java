3822ffe825bd7d0b4090eead4cb93d6e6db44ce5
robin.roger <robin.roger@epita.fr>
2026-04-15 16:09:35 +0200

    feat: add login form

--- src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package dev._xdbe.booking.creelhouse.infrastructure.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +10,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,38 +17,69 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-            .authorizeHttpRequests(auth -> auth
-                // Step 4a: add access control
-                // ...
-                // Step 4a: end
-                .anyRequest().permitAll()
-            )
-            // Step 4b: Add login form
-            // ...
-            // Step 4b: End of login form configuration
-            
-            .csrf((csrf) -> csrf
-                .ignoringRequestMatchers("/h2-console/**")
-            )
-            .headers(headers ->
-                headers.frameOptions(frameOptions ->
-                    frameOptions.disable()
-                )
-            )
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http
+        .authorizeHttpRequests(auth
+                               -> auth
+                                      // Step 4a: add access control
+                                      .requestMatchers("/dashboard")
+                                      .hasRole("ADMIN")
+                                      // Step 4a: end
+                                      .anyRequest()
+                                      .permitAll())
+        // Step 4b: Add login form
+        .formLogin(withDefaults())
+        // Step 4b: End of login form configuration
+
+        .csrf((csrf) -> csrf.ignoringRequestMatchers("/h2-console/**"))
+        .headers(
+            headers
+            -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+        .build();
+  }
+
+  // Step 3: add InMemoryUserDetailsManager
+  @Bean
+
+  public UserDetailsService userDetailsService() {
+
+    UserDetails admin =
+
+        User.builder()
+
+            .username("admin")
+
+            .password("{bcrypt}$2a$10$PVtPds1kSNQlYFHAZs4xe.YK.oRrEJgLw4KsN/"
+                      +
+
+                      "jSRNzJ4kn/Ac6ZS") // hash bcrypt
+
+            .roles("ADMIN")
+
             .build();
-    }
 
-    // Step 3: add InMemoryUserDetailsManager
-    // ...
-    // Step 3: end
+    UserDetails guest =
 
+        User.builder()
+
+            .username("guest")
+
+            .password("{bcrypt}$2a$10$k.ySNsMOKbsXAUp2Ka.UK.HST2kcdPI3f/"
+                      +
+
+                      "GaXzA899XQDxVUKg956") // hash bcrypt
+
+            .roles("GUEST")
+
+            .build();
+
+    return new InMemoryUserDetailsManager(admin, guest);
+  }
+  // Step 3: end
 }
